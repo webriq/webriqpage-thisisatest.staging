612cfa5fed1585e6d0113882cfa057325f88db69
allen_support <allen.support@webfactories.biz>
2016-03-03 14:51:56 +0800

    merge some css into one, add contact page, thank you page, sample page for the news, fixing some responsiveness

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@ wow.init();
 
 $(window).load(function(){
 
-	$('.main-nav li a').bind('click',function(event){
+	$('.main-nav li a, #header a').bind('click',function(event){
 		var $anchor = $(this);
 
 		$('html, body').stop().animate({
