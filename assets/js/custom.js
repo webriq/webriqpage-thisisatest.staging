494d810f215d7674afde0ba4d40dc4978da4b3c6
allen_support <allen.support@webfactories.biz>
2016-03-04 11:59:05 +0800

    remove some local js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,9 +29,12 @@ $(document).ready(function(e) {
 						$('.main-nav').slideToggle();
 						return false
 				});
+				$('.scrollspy').scrollSpy();
 });
 
 $(function(){
+
+		cbpBGSlideshow.init();
 
 		$('.lazy-wrapper').lazyLoader({
 				jsonFile: '../js/data.json',
@@ -56,6 +59,25 @@ $(function(){
 			   }
 			});
 		});
+
+  function stripTrailingSlash(str) {
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+
+  var url = window.location.pathname;
+  var activePage = stripTrailingSlash(url);
+
+  $('.main-nav li a').each(function(){
+    var currentPage = stripTrailingSlash($(this).attr('href'));
+
+    if (activePage == currentPage) {
+      $(this, 'a').addClass('active');
+    }
+  });
+
 
 
 		var d = document, s = d.createElement('script');
