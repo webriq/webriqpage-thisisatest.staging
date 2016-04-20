5a61d6ba5d2810906a29c93a2b4b5c270bf62892
allen_support <allen.support@webfactories.biz>
2016-04-20 14:13:06 +0800

    modify custom admin

--- assets/js/custom-admin.js
+++ assets/js/custom-admin.js
@@ -9,13 +9,6 @@ var logo = '/img/logo@xs.png';
 // Work properly. All your hacks put
 // Them inside this container instead.
 
-$('.cms.cms-inline-list').ready(function(e){
-
-  e.preventDefault();
-  $('<li class="cms cms-li cms-has-dropdown"><a class="cms cms-nav-link" href="/" target="_blank"><i class="cms-icon fa fa-link" aria-hidden="true"></i> VIEW</a></li>').insertBefore(".cms-right-menu .cms-inline-list li:first-child");
-
-});
-
 
 $( document ).ajaxComplete(function() {
 $('<li class="cms cms-li cms-has-dropdown"><a class="cms cms-nav-link" href="/" target="_blank"><i class="cms-icon fa fa-link" aria-hidden="true"></i> VIEW</a></li>').insertBefore(".cms-right-menu .cms-inline-list li:first-child");
